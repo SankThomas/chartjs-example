473feda3ad34c369a76d647419ac765c8727e6e9
Thomas Sankara <thomassankara74@gmail.com>
2021-09-14 09:47:12 +0300

    how to use chartjs in a react appliction

--- index.js
+++ index.js
@@ -25,3 +25,49 @@ const config = {
 }
 
 const myChart = new Chart(document.getElementById("myChart"), config)
+
+/*
+Intergrating chartjs in a react app (from https://www.educative.io/edpresso/how-to-use-chartjs-to-create-charts-in-react)
+
+// npm install chartjs react-chartjs-2
+
+import React from "react"
+import { Line } from "react-chartjs-2"
+
+const state = {
+  labels: ["January", "February", "March", "April", "May"],
+  datasets: [
+    {
+      label: "Rainfall",
+      fill: false,
+      lineTension: 0.5,
+      backgroundColor: "rgba(75,192,192,1)",
+      borderColor: "rgba(0,0,0,1)",
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56],
+    },
+  ],
+}
+
+export default function App() {
+  return (
+    <div>
+      <Line
+        data={state}
+        options={{
+          title: {
+            display: true,
+            text: "Average Rainfall per month",
+            fontSize: 20,
+          },
+          legend: {
+            display: true,
+            position: "right",
+          },
+        }}
+      />
+    </div>
+  )
+}
+
+*/
